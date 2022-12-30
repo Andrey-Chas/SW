5e55464c6445c7e2d77ef8dc137fdff2754bf455
Andrey Chasovskoy <andrchas777@gmail.com>
2022-12-30 16:05:04 +0200

    code with function

--- function_for_comb.py
+++ function_for_comb.py
@@ -4,4 +4,10 @@ from math import comb
 
 
 def wrapper(arg1, arg2):
-    pass
+    if not(isinstance(arg1, int) and isinstance(arg2, int)):
+        msg = "must be int, given " + str(type(arg1))
+        raise TypeError(msg)
+    return comb(int(arg1), int(arg2))
+
+
+assert(wrapper(10, 5)) == 252
